efe3a2b91bedd8a3c54b75d34041f91a00b43096
William <wwz2003@nyu.edu>
2026-02-22 02:11:59 -0800

    Remove debug toolbar for deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-] + debug_toolbar_urls()
+] 
+# + debug_toolbar_urls()
