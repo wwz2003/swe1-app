231670dd1f0dd76a163c0559ac270533bbf34995
William <wwz2003@nyu.edu>
2026-02-22 01:42:39 -0800

    staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Internal IPS (django-debug-toolbar)
 INTERNAL_IPS = [
