bf104fd8cfab64cd61a7d48ad070559118c25e25
Ian <43066864+IanC13@users.noreply.github.com>
2026-03-10 18:06:08 -0400

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -125,3 +125,7 @@ class QuestionDetailViewTests(TestCase):
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+    def test_dummy(self):
+        self.assertTrue(True)
+
