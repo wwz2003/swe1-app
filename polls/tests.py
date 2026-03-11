add33a10cff71b6692350abc625ef98cf0ba83bf
Ian <43066864+IanC13@users.noreply.github.com>
2026-03-10 23:31:11 -0400

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -127,5 +127,5 @@ class QuestionDetailViewTests(TestCase):
         self.assertContains(response, past_question.question_text)
 
     def test_dummy(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
 
